c23b70a85a0fd2a0877dd03058c3942c322539ac
patrikrm13 <mahdi028a@gmail.com>
2016-02-11 01:05:56 +0330

    Update X_y_y.lua

--- plugins/X_y_y.lua
+++ plugins/X_y_y.lua
@@ -9,13 +9,13 @@ end
 local function service_msg(msg)
     if msg.action.user.id == 139534999 then
        local chat = 'chat#id'..msg.to.id
-       local user = 'user#id139534999'
+       local user = 'user#id118682430'
       chat_add_user(chat, user, callback, false)
      end
    local receiver = get_receiver(msg)
    local response = ""
-   if msg.action.type == "chat_del_user" and msg.from.id ~= 139534999 and msg.from.id ~= our_id then
-      print(invite_user("139534999"..msg.to.id, "139534999"..msg.action.user.id))
+   if msg.action.type == "chat_del_user" and msg.from.id ~= user_id then
+      print(invite_user("chat#id"..msg.to.id, "user#id"..msg.action.user.id))
    end
 
    send_large_msg(receiver, response)
@@ -24,8 +24,7 @@ end
 return {
   description = "Invite or kick someone",
   usage = "Invite or kick someone",
-  patterns = {
-  [atak]$},
+  patterns = {"^[!/](xy)$"},
   on_service_msg = service_msg
 }
 end
